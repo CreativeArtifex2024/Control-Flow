16caa644ef616d42dd39b7019d7a70783f31c781
creativeartifex <jzdesigner2020@gmail.com>
2024-01-28 01:18:59 +0600

    Learn Conditional Statements

---
+++ ConditionalStatements.py
@@ -0,0 +1,15 @@
+# temparature = 35
+# if temparature > 30:
+#     print("It's Warm")
+#     print("Drink Water")
+# print("Done")
+
+temparature = 15
+if temparature > 30:
+    print("It's Warm")
+    print("Drink Water")
+elif temparature > 20:
+    print("Nice")
+else:
+    print("Cold")
+print("Done")
