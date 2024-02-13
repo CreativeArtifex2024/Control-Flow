038c133018d6031407e2aa756ebdba89d895fa84
creativeartifex <jzdesigner2020@gmail.com>
2024-02-14 01:50:41 +0600

    ForElse

---
+++ ForElse.py
@@ -0,0 +1,17 @@
+# successfull = True
+# for number in range(3):
+#     print("Attempt")
+#     if successfull:
+#         print("successfull")
+#         break
+# else:
+#     print("Attempted three times and fail")
+
+successfull = False
+for number in range(3):
+    print("Attempt")
+    if successfull:
+        print("successfull")
+        break
+else:
+    print("Attempted three times and fail")
